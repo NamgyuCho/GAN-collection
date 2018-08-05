006e827e22a31885099836f67078d45ea09f09b6
NamgyuCho <southq82@gmail.com>
2018-08-05 15:51:20 +0900

    1st update

--- InfoGAN/trainer.py
+++ InfoGAN/trainer.py
@@ -65,7 +65,7 @@ class Trainer:
         optimG = optim.Adam([{'params':self.G.parameters()}, {'params':self.Q.parameters()}],
                             lr=0.001, betas=(0.5, 0.99))
 
-        dataset = dset.MNIST('./dataset', transform=transforms.ToTensor())
+        dataset = dset.MNIST('./dataset', transform=transforms.ToTensor(), downlaod=True)
         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True, num_workers=1)
 
         # fixed random variables
